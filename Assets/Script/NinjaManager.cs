6bb2e5282d8736c79c2c904741036e9f58a58385
Robert <chen1fan@icloud.com>
2020-04-04 04:00:07 -0400

    completely into the MVC pattern

---
+++ Assets/Script/NinjaManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NinjaManager : MonoBehaviour
+{
+
+    public Model theModel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        theModel = new Model();
+        theModel.SetView(GetComponent<View>());
+        
+    }
+
+    
+}
---
+++ Assets/Script/View.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Display
+public class View: MonoBehaviour   
+    {   
+    public Animator animator; // Animation is specific to view.
+    SpriteRenderer sprite;
+
+    public void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+    public void Update()
+    {
+        float smallMove = 0.0001f;
+        if (Controller.xMove < -smallMove)
+        {
+            sprite.flipX = true;
+        }
+        else if (Controller.xMove> smallMove)
+        {
+            sprite.flipX = false;
+        }
+
+    }
+
+    public void SetGrounded(bool isGrounded)
+    {
+        animator.SetBool("grounded", isGrounded);
+    }
+
+    public void SetAnimationSpeed(float speed)
+    {
+        animator.SetFloat("speed", Mathf.Abs(speed));
+    }
+}
