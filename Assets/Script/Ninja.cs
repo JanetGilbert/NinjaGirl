084603d9a8407994b7d07d07e390b75917d83d3b
crownella <35846857+crownella@users.noreply.github.com>
2020-04-16 20:20:18 -0400

    Fixed Comments

--- Assets/Script/Ninja.cs
+++ Assets/Script/Ninja.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class Ninja : MonoBehaviour
 {
-    float moveX;
-    bool grounded;
-    bool jumping;
+    float moveX; //X axis Input
+    bool grounded; //checks if player is collidng with a platform
+    bool jumping; //checks if player is currently jumping
 
-    float jumpTime;
+    float jumpTime; //used to control the time spent jumping
 
-
+    [SerializeField, Tooltip("Speed on X Axis")]
     public float speed = 1.0f;
+    [SerializeField, Tooltip("Jump Speed on Y Axis")]
     public float jumpspeed = 1.0f;
+    [SerializeField, Tooltip("Max time the Ninja can remain Jumping")]
     public float maxJumpTime = 1.0f;
 
+    //Ninja Object Components
     Animator animator;
     Rigidbody2D rb;
     BoxCollider2D boxCollider;
@@ -22,6 +25,7 @@ public class Ninja : MonoBehaviour
 
     void Start()
     {
+        //get instances of each necessary component
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
@@ -31,35 +35,36 @@ public class Ninja : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Is platform below us?
+        //Check for platform under player using a raycast
         float selfHeightOffset = (boxCollider.size.y / 2.0f) + 0.1f;
         float rayLen =  0.05f;
         Vector2 pos2D = (Vector2)transform.position;
        // Debug.DrawRay(pos2D - (Vector2.up * selfHeightOffset), -Vector2.up * rayLen, Color.red);
         RaycastHit2D hit = Physics2D.Raycast(pos2D - (Vector2.up * selfHeightOffset), -Vector2.up, rayLen);
 
+        //set grounded to false every frame
         grounded = false;
         if (hit.collider != null)
         {
             if (hit.collider.CompareTag("Platform"))
             {
-                grounded = true;
+                grounded = true; //set grounded to true if a platform is hit
             }
         }
 
-        // Jumping
+        //Input Management
         if (grounded)
         {
-            moveX = Input.GetAxis("Horizontal");
+            moveX = Input.GetAxis("Horizontal"); // get X Axis input 
 
-
+            //if jump time has reached 0, stop applying the jump force
             if (jumpTime < 0.0f)
             {
                 jumping = false;
             }
         }
 
-
+        //get jump input
         bool jumpPressed = Input.GetButtonDown("Jump");
         if (grounded && jumpPressed && !jumping)
         {
@@ -69,7 +74,7 @@ public class Ninja : MonoBehaviour
 
         jumpTime -= Time.deltaTime;
 
-        // Reverse horizontal 
+        //Sprite Direction based off minimam movement
         float smallMove = 0.0001f;
         if (moveX < -smallMove)
         {
@@ -80,23 +85,26 @@ public class Ninja : MonoBehaviour
             sprite.flipX = false;
         }
 
-        // Animator parameters
+        //Set Animator Vars
         animator.SetFloat("speed", Mathf.Abs(moveX));
         animator.SetBool("grounded", grounded);
     }
 
-    // Physic stuff
+    
+    //Fixed Update is used for in Game Physics
     private void FixedUpdate()
     {
         if (jumping)
         {
             if (jumpTime > 0.0f)
             {
+                //if jump time has not reached 0, set the players Y veloctiy to jumpspeed
                 rb.velocity = new Vector2(moveX * speed, jumpspeed);
             }
         }
         else if (grounded)
         {
+            //set the X velocity to X axis speed mutiplied by the X axis input
             rb.velocity = new Vector2(moveX * speed, 0.0f);
         }
     }
